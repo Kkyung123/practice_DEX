ee0c46daafd66444e2205ca38667a9cd5973a43e
Kkyung123 <heekyung.shin@dream-academy.io>
2022-09-23 20:42:25 +0900

    add code

--- src/dex.sol
+++ src/dex.sol
@@ -4,7 +4,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import "./Math.sol";
 
-contract DEX is IERC20, ERC20 {
+contract DEXs is IERC20, ERC20 {
 
     address public tokenX;
     address public tokenY;
--- test/dex.t.sol
+++ test/dex.t.sol
@@ -2,85 +2,82 @@
 pragma solidity ^0.8.0;
 
 import "../src/dex.sol";
-import "../forge-std/src/console2.sol";
-import "../forge-std/src/Test.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "../src/ERC20Mintable.sol";
+import "forge-std/src/console2.sol";
+import "forge-std/src/Test.sol";
 
 contract DexTest is Test{
-    DEX public Dex1;
+    DEXs public Dex1;
     ERC20Mintable public token1;
     ERC20Mintable public token2;
-    DEX public token;
+    DEXs public token;
+    
+    address internal one = address(1);
+    address internal two = address(2);
+    address internal three = address(3);
+    address internal four = address(4);
 
-    address internal spring = address(1);
-    address internal summer = address(2);
-    address internal fall = address(3);
-    address internal winter = address(4);
-    address internal hee = address(5);
 
     function setUp() public{
         token1 = new ERC20Mintable("kkyongi", "KYU1");
         token2 = new ERC20Mintable("kkyongii", "KYU2");
 
-        Dex1 = new DEXsol(address(token1), address(token2));
+        Dex1 = new DEXs(address(token1), address(token2));
 
-        token1.mint(spring, 200);
-        token2.mint(spring, 200);
+        token1.mint(one, 200);
+        token2.mint(one, 200);
 
-        token1.mint(summer, 300);
-        token2.mint(summer, 50);
+        token1.mint(two, 300);
+        token2.mint(two, 50);
 
-        token1.mint(fall, 200);
-        token2.mint(fall, 0);
+        token1.mint(three, 200);
+        token2.mint(three, 0);
 
-        token1.mint(winter, 0);
-        token2.mint(winter, 200);
+        token1.mint(four, 0);
+        token2.mint(four, 200);
+      
+    }
+
+     function testFirstAddLiquidity() public {
+         vm.startPrank(one);
+         token1.approve(address(Dex1), 200);
+         token2.approve(address(Dex1), 200);
         
-        token1.mint(hee, 0);
-        token2.mint(hee, 0);
-    }
+         Dex1.addLiquidity(100, 100, 0);
 
-    function testFirstAddLiquidity() public {
-        vm.startPrank(spring);
-        token1.approve(address(Dex1), 200);
-        //vm.prank(spring);
-        token2.approve(address(Dex1), 200);
+         assertEq(Dex1.balanceOf(one), 100); 
+         assertEq(token1.balanceOf(one), 100); 
+         assertEq(token2.balanceOf(one), 100); 
+
+         assertEq(token1.balanceOf(address(Dex1)), 100); 
+         assertEq(token2.balanceOf(address(Dex1)), 100); 
+     }
+
+     function testFailFirstAddLiauidity() public {
+         vm.startPrank(one);
+         token1.approve(address(Dex1), 100);
+         token2.approve(address(Dex1), 100);
+
+         Dex1.addLiquidity(100, 50, 0);
+         vm.expectRevert("The proportion is broken");
+     }
+
+     function testRemoveLiquidity() public {
+         testFirstAddLiquidity();
+
+         Dex1.removeLiquidity(50, 0, 0);
         
-        Dex1.addLiquidity(100, 100, 0);
-
-        assertEq(Dex1.balanceOf(spring), 100); //lptoken 개수
-        assertEq(token1.balanceOf(spring), 100); // 200개 중에 100개 넣음 -> 100
-        assertEq(token2.balanceOf(spring), 100); // 200개 중에 100개 넣음 -> 100 
-
-        assertEq(token1.balanceOf(address(Dex1)), 100); //풀에 들어있는 token1의 개수 : 100개
-        assertEq(token2.balanceOf(address(Dex1)), 100); // 풀에 들어있는 token2의 개수 : 100개
-    }
-
-    function testFailFirstAddLiauidity() public {
-        vm.startPrank(spring);
-        token1.approve(address(Dex1), 100);
-        token2.approve(address(Dex1), 100);
-
-        Dex1.addLiquidity(100, 50, 0);
-        vm.expectRevert("The proportion is broken");
-    }
-
-    function testRemoveLiquidity() public {
-        testFirstAddLiquidity();
-
-        //vm.prank(spring);
-        Dex1.removeLiquidity(50, 0, 0);
-        
-        assertEq(token1.balanceOf(address(spring)), 150); //spring의 token0 잔고 + 다시 돌려받은 토큰 수
-        assertEq(token2.balanceOf(address(spring)), 150);
-    }
+         assertEq(token1.balanceOf(address(one)), 150); 
+         assertEq(token2.balanceOf(address(one)), 150);
+     }
     
-    function testSwap() public{
-        testFirstAddLiquidity();
-        //vm.prank(spring);
-        Dex1.swap(50, 0, 0);
-        assertEq(token1.balanceOf(address(spring)), 50);
-        assertEq(token2.balanceOf(address(spring)), 134);
-    }
+     function testSwap() public{
+         testFirstAddLiquidity();
+
+         Dex1.swap(50, 0, 0);
+         assertEq(token1.balanceOf(address(one)), 50);
+         assertEq(token2.balanceOf(address(one)), 134);
+     }
+     
     receive() external payable {}
 }
